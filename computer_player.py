3b506a05dd3c96f45978d2a42013b9fa10e0389a
Nils Manni <nilsmanni@gmail.com>
2020-10-09 15:33:35 +0200

    amelioration 1 de computer player avec ajustement de min et max

---
+++ computer_player.py
@@ -0,0 +1,19 @@
+import random
+
+from guess_my_number import MIN, MAX, guessmachine
+
+if __name__ == '__main__':
+    min = MIN
+    max = MAX
+    guess_nachine = guessmachine()
+    while True:
+        attempt = random.randint(min,max)
+        result = guess_nachine.guess(attempt)
+        print('tried %d : %s' % (attempt, result))
+        if result == 'found':
+            print('finished in %d attempts.'% guess_nachine.number_of_attempt)
+            break
+        elif result == 'too low' :
+            min = attempt + 1
+        else :
+            max = attempt - 1
